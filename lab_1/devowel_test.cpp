cfd675c6df7a3c3a940a594a904d3f459dbc472a
Polina <polina.dosegaeva@mail.ru>
2023-09-29 14:54:25 +0300

    добавила тест с 2-мя гласными

--- lab_1/devowel_test.cpp
+++ lab_1/devowel_test.cpp
@@ -7,3 +7,8 @@ TEST(DevowelTest, AllVowels) {
   string s = devowel(string("aaaaa"));
   EXPECT_EQ(s.length(), 0);
 }
+
+TEST(DevowelTest, TwoVowels) {
+	string s = devowel(string("paul"));
+	EXPECT_EQ(s.length(), 2);
+}
