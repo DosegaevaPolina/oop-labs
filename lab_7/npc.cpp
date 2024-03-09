3ad7556dfd52480cc9f44ec01cb529dcba1d220e
Polina <polina.dosegaeva@mail.ru>
2024-03-09 17:31:20 +0300

    лабораторная #7 - #6, только NPC имеют скорость и радиус атаки

---
+++ lab_7/npc.cpp
@@ -0,0 +1,42 @@
+#include "npc.hpp"
+
+std::string Spawner::rand_name() {
+  const char alphanum[] = "0123456789"
+                          "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                          "abcdefghijklmnopqrstuvwxyz";
+  std::string tmp_s;
+  // 4 <= len <= 10
+  int len = 4 + rand() % 7;
+  tmp_s.reserve(len);
+
+  for (int i = 0; i < len; ++i) {
+    tmp_s += alphanum[rand() % (sizeof(alphanum) - 1)];
+  }
+
+  return tmp_s;
+}
+
+int Spawner::rand_x() const {
+  return x_range.first + (rand() % (x_range.second - x_range.first + 1));
+}
+
+int Spawner::rand_y() const {
+  return y_range.first + (rand() % (y_range.second - y_range.first + 1));
+}
+
+Spawner* Spawner::from_string(string type, int_pair x_range, int_pair y_range) {
+  if (type == Bear::type_name)
+    return new BearSpawner(x_range, y_range);
+  if (type == Elf::type_name)
+    return new ElfSpawner(x_range, y_range);
+  if (type == Rogue::type_name)
+    return new RogueSpawner(x_range, y_range);
+  return nullptr;
+}
+
+int NPC::dist2(const NPC *other) const {
+  return (x - other->x) * (x - other->x) + (y - other->y) * (y - other->y);
+}
+
+int NPC::dice_roll(int max) { return 1 + (rand() % max); }
+
---
+++ lab_7/npc.hpp
@@ -0,0 +1,214 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <unistd.h>
+
+using std::string;
+
+class NPC;
+class Bear;
+class Elf;
+class Rogue;
+
+// интерфейс Посетителя объявляет набор методов посещения, соответствующих
+// классам компонентов. Сигнатура метода посещения позволяет посетителю 
+// определить конкретный класс компонента, с которым он имеет дело
+class Visitor {
+public:
+  virtual ~Visitor() {}
+  virtual void VisitBear(Bear *element, NPC *other) = 0;
+  virtual void VisitElf(Elf *element, NPC *other) = 0;
+  virtual void VisitRogue(Rogue *element, NPC *other) = 0;
+};
+
+
+// объявляем операции, которые должны выполнять все конкретные NPC
+class NPC {
+protected:
+public:
+  string name;
+  int x, y;
+  bool alive;
+
+  NPC(const string &name, int x, int y) : name(name), x(x), y(y), alive(true) {}
+
+  virtual ~NPC() {}
+
+  virtual const string& type_str() const = 0;
+  virtual const int get_velocity() const = 0;
+  virtual const int get_range() const = 0;
+  virtual void Accept(Visitor *visitor, NPC *other) = 0;
+
+  int dist2(const NPC *other) const;
+  static int dice_roll(int max);
+
+  friend std::ostream &operator<<(std::ostream &os, const NPC &npc)  {
+    os << npc.type_str() << " " << npc.name << " "
+              << npc.x << " "  << npc.y;
+    return os;
+  }
+};
+
+
+// конкретные NPC предоставляют различные реализации интерфейса
+class Bear : public NPC {
+public:
+  inline static const string type_name = "Bear";
+  inline static const int velocity = 5;
+  inline static const int range = 10;
+
+  Bear(string name, int x, int y) : NPC(name, x, y) {}
+
+  const string& type_str() const override { return type_name; }
+
+  const int get_velocity() const override { return velocity; }
+  
+  const int get_range() const override { return range; }
+
+  void Accept(Visitor *visitor, NPC *other) override {
+    // std::cout << "Bear accepting visitor\n";
+    visitor->VisitBear(this, other);
+  }
+};
+
+class Elf : public NPC {
+public:
+  inline static const string type_name = "Elf";
+  inline static const int velocity = 10;
+  inline static const int range = 50;
+
+  Elf(string name, int x, int y) : NPC(name, x, y) {}
+
+  const string &type_str() const override { return type_name; }
+
+  const int get_velocity() const override { return velocity; }
+  
+  const int get_range() const override { return range; }
+
+  void Accept(Visitor *visitor, NPC *other) override {
+    // std::cout << "Elf accepting visitor\n";
+    visitor->VisitElf(this, other);
+  }
+};
+
+class Rogue : public NPC {
+public:
+  inline static const string type_name = "Rogue";
+  inline static const int velocity = 10;
+  inline static const int range = 10;
+
+  Rogue(string name, int x, int y) : NPC(name, x, y) {}
+
+  const string &type_str() const override { return type_name; }
+
+  const int get_velocity() const override { return velocity; }
+  
+  const int get_range() const override { return range; }
+
+  void Accept(Visitor *visitor, NPC *other) override {
+    // std::cout << "Rogue accepting visitor\n";
+    visitor->VisitRogue(this, other);
+  }
+};
+
+using int_pair = std::pair<int,int>;
+// класс Spawner объявляет фабричный метод, который должен возвращать объект класса NPC
+// подклассы Spawner'a предоставляют реализацию этого метода
+class Spawner {
+protected:
+  int_pair x_range, y_range;
+
+  static std::string rand_name();
+  int rand_x() const;
+  int rand_y() const;
+
+public:
+  Spawner(int_pair x_range, int_pair y_range) : x_range(x_range), y_range(y_range) {}
+  virtual ~Spawner(){};
+  virtual NPC* spawn(string name, int x, int y) const = 0;
+  virtual NPC* spawn_random() const = 0;
+  static Spawner* from_string(string, int_pair, int_pair);
+};
+
+// конкретные Spawner'ы переопределяют фабричный метод для того, чтобы изменить тип получаемого NPC
+class BearSpawner : public Spawner {
+  using Spawner::Spawner;
+public:
+  NPC *spawn(string name, int x, int y) const override {
+    return new Bear(name, x, y);
+  }
+  NPC *spawn_random() const override {
+    return new Bear(rand_name(), rand_x(), rand_y());
+  }
+};
+
+class ElfSpawner : public Spawner {
+  using Spawner::Spawner;
+public:
+  NPC *spawn(string name, int x, int y) const override {
+    return new Elf(name, x, y);
+  }
+  NPC *spawn_random() const override {
+    return new Elf(rand_name(), rand_x(), rand_y());
+  }
+};
+
+class RogueSpawner : public Spawner {
+  using Spawner::Spawner;
+public:
+  NPC *spawn(string name, int x, int y) const override {
+    return new Rogue(name, x, y);
+  }
+  NPC *spawn_random() const override {
+    return new Elf(rand_name(), rand_x(), rand_y());
+  }
+};
+
+// конкретные Посетители реализуют несколько версий одного и того же алгоритма,
+// которые могут работать со всеми классами конкретных компонентов.
+class FightVisitor : public Visitor {
+private:
+  bool kills(NPC *npc, NPC *other) {
+    if (npc->type_str()   == Bear::type_name &&
+        other->type_str() == Elf::type_name)
+      return true;
+    if (npc->type_str()   == Elf::type_name &&
+        other->type_str() == Rogue::type_name)
+      return true;
+    if (npc->type_str()   == Rogue::type_name &&
+        other->type_str() == Rogue::type_name)
+      return true;
+    
+    return false;
+  }
+
+  void VisitNPC(NPC *npc, NPC *other) {
+    if (npc->dist2(other) <= npc->get_range() * npc->get_range()) {
+      if (kills(npc,other)) {
+        int attack = NPC::dice_roll(6);
+        int defence = NPC::dice_roll(6);
+        // if (attack > defence)
+          other->alive = false;
+      }
+    }
+    if (npc->dist2(other) <= other->get_range() * other->get_range()) {
+      if (kills(other,npc)) {
+        int attack = NPC::dice_roll(6);
+        int defence = NPC::dice_roll(6);
+        // if (attack > defence)
+          npc->alive = false;
+      }
+    }
+  }
+
+public:
+  ~FightVisitor() override {}
+
+  void VisitBear(Bear *bear, NPC *other) override { VisitNPC(bear, other); }
+
+  void VisitElf(Elf *elf, NPC *other) override { VisitNPC(elf, other); }
+
+  void VisitRogue(Rogue *rogue, NPC *other) override { VisitNPC(rogue, other); }
+};
+
